aa34a62a465c762af0f2d2dc662afccf8a5df3df
David Montoya <davidmoncas@gmail.com>
2020-06-28 19:40:12 +0200

    images

---
+++ Assets/Scripts/distance_manager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class distance_manager : MonoBehaviour
+{
+
+    public int numberNPCS;
+
+    public Transform[] NPCS;
+    public float[] distances;   //radio of sight of the npc
+
+    public Transform player;
+    private int maxDist=20;
+    public LayerMask ignoreLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        foreach (Transform npc in NPCS)
+        {
+
+            Vector2 dir = npc.position - player.position;
+
+            RaycastHit2D hit = Physics2D.Raycast(player.position, dir , maxDist, ~ignoreLayer);
+            if (hit.collider != null)
+            {
+               // print(hit.collider);
+            }
+
+
+
+        }
+
+
+    }
+}
+
+
--- Assets/Scripts/moveCharacter.cs
+++ Assets/Scripts/moveCharacter.cs
@@ -8,6 +8,8 @@ public class moveCharacter : MonoBehaviour
     public float speed = 0.5f;
     private Rigidbody2D rb;
     Vector2 movement;
+    int direction;
+    float velocity;
 
     void Start()
     {
@@ -20,8 +22,21 @@ public class moveCharacter : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
-        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+        if (movement.magnitude > 0)
+        {
+            this.GetComponent<Animator>().SetFloat("horizontal", movement.x);
+            this.GetComponent<Animator>().SetFloat("vertical", movement.y);
+        }
 
+        this.GetComponent<Animator>().SetBool("moving", movement.magnitude > 0);
+
+ 
+
+
+        Debug.Log(rb.velocity.magnitude);
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+        
+        
     }
 
 }
