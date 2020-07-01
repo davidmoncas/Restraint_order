ad242d7c7b7c26080f82fd21a0fc59c096038977
zohair34 <36851887+zohair34@users.noreply.github.com>
2020-07-02 01:18:35 +0200

    added new chase scene

---
+++ Assets/AstarPathfindingProject/Core/AI/EnemyGraphics.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyGraphics : MonoBehaviour
+{
+
+    public AIPath aiPath;
+    public Transform spawnPoint;
+    public AIDestinationSetter aiDestin;
+    public Transform player;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        print(Vector3.Distance(player.position, transform.position));
+        if (Vector3.Distance(player.position, transform.position) < 5) aiDestin.target = player;
+        else aiDestin.target = spawnPoint;
+
+        if (aiPath.desiredVelocity.x >= 0.01f) transform.localScale = new Vector3(-1,1,1);
+        else if(aiPath.desiredVelocity.x<=-0.01f) transform.localScale = new Vector3(1, 1, 1);
+    }
+}
