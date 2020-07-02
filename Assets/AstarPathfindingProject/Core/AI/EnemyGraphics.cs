def5f37cb00658bc25699e402797a9539e3b77fc
zohair34 <36851887+zohair34@users.noreply.github.com>
2020-07-02 20:28:33 +0200

    added chase animation walking

--- Assets/AstarPathfindingProject/Core/AI/EnemyGraphics.cs
+++ Assets/AstarPathfindingProject/Core/AI/EnemyGraphics.cs
@@ -10,8 +10,9 @@ public class EnemyGraphics : MonoBehaviour
     public Transform spawnPoint;
     public AIDestinationSetter aiDestin;
     public Transform player;
+    public Animator animator;
 
-
+    bool switchDir = false;
     // Update is called once per frame
     void Update()
     {
@@ -20,7 +21,36 @@ public class EnemyGraphics : MonoBehaviour
         if (Vector3.Distance(player.position, transform.position) < 5) aiDestin.target = player;
         else aiDestin.target = spawnPoint;
 
-        if (aiPath.desiredVelocity.x >= 0.01f) transform.localScale = new Vector3(-1,1,1);
-        else if(aiPath.desiredVelocity.x<=-0.01f) transform.localScale = new Vector3(1, 1, 1);
+
+
+
+        if (aiPath.desiredVelocity.x >= 0.01f) {
+          //  transform.localScale = new Vector3(-1, 1, 1);
+            if(!animator.GetCurrentAnimatorStateInfo(0).IsName("walk_left"))
+            animator.Play("walk_left");
+
+        }
+        else if (aiPath.desiredVelocity.x <= -0.01f) {
+           // transform.localScale = new Vector3(1, 1, 1);
+            if (!animator.GetCurrentAnimatorStateInfo(0).IsName("walk_right"))
+                animator.Play("walk_right");
+        }
+
+
+        /*
+      if (aiPath.desiredVelocity.y >= 0.1f)
+        {
+            //  transform.localScale = new Vector3(-1, 1, 1);
+            if (!animator.GetCurrentAnimatorStateInfo(0).IsName("walk_up"))
+                animator.Play("walk_up");
+
+        }
+        else if (aiPath.desiredVelocity.y <= -0.1f)
+        {
+            // transform.localScale = new Vector3(1, 1, 1);
+            if (!animator.GetCurrentAnimatorStateInfo(0).IsName("walk_down"))
+                animator.Play("walk_down");
+        }
+     */  
     }
 }
